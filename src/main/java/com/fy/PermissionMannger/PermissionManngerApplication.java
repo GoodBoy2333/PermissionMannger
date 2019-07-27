7d54777e1e76002ac6272482d7c321d97f2c31c1
GoodBoy2333 <770488542@qq.com>
2019-07-27 13:38:25 +0800

    删除注释代码

--- src/main/java/com/fy/PermissionMannger/PermissionManngerApplication.java
+++ src/main/java/com/fy/PermissionMannger/PermissionManngerApplication.java
@@ -13,26 +13,4 @@ public class PermissionManngerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PermissionManngerApplication.class, args);
     }
-
-    /*@Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        // 1、需要先定义一个 convert 转换消息的对象;
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-
-        //2、添加fastJson 的配置信息，比如：是否要格式化返回的json数据;
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-
-        //3、在convert中添加配置信息.
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-
-        //处理中文乱码问题
-
-        List<MediaType> fastMediaTypes = new ArrayList<>();
-        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        fastConverter.setSupportedMediaTypes(fastMediaTypes);
-
-        HttpMessageConverter<?> converter = fastConverter;
-        return new HttpMessageConverters(converter);
-    }*/
 }
